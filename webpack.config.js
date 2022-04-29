ac9360a9536900ff124f68d342c220adee7f22a1
samuelm <smbra33@gmail.com>
2022-04-29 15:06:59 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+
+    entry: "./frontend/widgets.jsx",
+
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'bundle.js'
+    },
+
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".js",".jsx", "*"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+            }
+        ]   
+    }
+};
